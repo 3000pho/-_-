37a78d425ceeb1ecd8a596acc6136245ffe45fe0
grachegrache <hoonse07@naver.com>
2017-04-10 05:15:01 +0900

    first push
    1. 캐릭터 움직임
    2. 카메라 이동, 회전

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+	public CharMove target;
+
+	private float dist_h;
+	private float dist_v;
+	//private float followSpeed;
+	private float turnSpeed;
+	private float rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+		dist_h = 5;
+		dist_v = 5;
+		//followSpeed = target.moveSpeed - 0.5f;
+		turnSpeed = 5f;
+		rotateSpeed = 200f;
+		follow ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LateUpdate(){
+		//follow the target behind if movements exists
+		if(target.movement.magnitude != 0){
+			//change direction smoothly
+			follow();
+		}
+
+		//rotate camera for input
+		float mouseX = Input.GetAxis("Mouse X");
+		transform.RotateAround (target.transform.position, Vector3.up, mouseX * rotateSpeed * Time.deltaTime);
+
+		//view the target
+		transform.LookAt (target.transform);
+	}
+
+	void follow(){
+		float currYAngle = Mathf.LerpAngle (transform.eulerAngles.y, target.transform.eulerAngles.y, turnSpeed * Time.deltaTime);
+		Quaternion targetRotate = Quaternion.Euler (0, currYAngle, 0);
+
+		transform.position = target.transform.position - (targetRotate * Vector3.forward * dist_h) + Vector3.up * dist_v;
+		//transform.position = Vector3.Lerp (transform.position, camPos, followSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/CharMove.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMove : MonoBehaviour {
+
+	public enum CharState
+	{
+		idle = 0,
+		walk,
+	}
+
+	public float moveSpeed;
+	public float rotateSpeed;
+	public Vector3 movement;
+
+	private Rigidbody rb;
+	private Animator animator;
+	private CharState state = CharState.idle;
+	//private CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		animator = GetComponent<Animator> ();
+		//controller = GetComponent<CharacterController> ();
+		movement = new Vector3 (0, 0, 0);
+		moveSpeed = 1f;
+		rotateSpeed = 6f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		float h =  Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+		movement.Set (h, 0.0f, v);
+		//rotate input based charater's direction
+		movement = Quaternion.Euler (0, transform.eulerAngles.y, 0) * movement;
+		movement *= moveSpeed * Time.deltaTime;
+
+		//movements exists
+		if (movement.magnitude != 0) {
+			state = CharState.walk;
+
+			Walk (movement);
+			Turn (movement);
+
+		} else {
+			state = CharState.idle;	
+		}
+
+		animator.SetInteger ("state", (int)state);
+	}
+
+	void Walk(Vector3 movement){
+		rb.MovePosition (transform.position + Vector3.Slerp(transform.forward, movement, 0.99f));
+		//controller.Move (movement);
+	}
+
+	void Turn(Vector3 movement){
+		Quaternion newRotate = Quaternion.LookRotation (movement);
+		rb.rotation = Quaternion.Slerp (rb.rotation, newRotate, rotateSpeed * Time.deltaTime);
+
+	}
+}
